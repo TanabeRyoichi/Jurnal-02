bca0079b001135bee32ddafb90f09c4940fcde29
TanabeRyoichi <71366913+TanabeRyoichi@users.noreply.github.com>
2020-09-16 13:23:53 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama:Tanabe Ryoichi");
+		System.out.println("Nim:6706204092");
+		System.out.println("Kelas:D3IF4404");
+	}
+}
